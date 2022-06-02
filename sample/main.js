73b28d4261423615bdba965650b295866ca9fa44
Jonas Wanner <jonas@wanner.com>
2022-06-02 22:02:11 +0200

    feat: improve sample

--- sample/main.js
+++ sample/main.js
@@ -1,28 +1,44 @@
-import { render } from '../src/cssed.js'
+import {render} from '../src/cssed.js'
 
 render(
     document.querySelector('#app'),
     `
-    .wrapper {
-        height: 400px;
-        width: 600px;
-        background: black;
+    .wrapper[div] {
         position: fixed;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%,-50%);
-        padding: 40px;
-    } 
-    
-    .wrapper .title[h1] {
-        color: white;
-        content: "Hoihoi";
-        font-size: 100px;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;   
+        background: black;
     }
     
-    .wrapper .content[p] {
-        content: "Da isch so sau unnötig aber unglaublich lustig";
-        color: white;
+    .wrapper .content {
+        width: 600px;
+        padding: 30px 30px 26px;
+        border: 3px solid white;
+        border-radius: 5px;
+        background: #cecece;
+        font-family: monospace;
+        margin: 40px;
+    }
+    
+    .wrapper .content .title[h1] {
+        color: black;
+        margin-top: 2px;
+        margin-bottom: 8px;
+        font-size: 40px;
+        content: "cssed";
+    }
+    
+    .wrapper .content .message[p] {
+        color: #6a6a6a;
+        margin-bottom: 0;
+        font-size: 16px;
+        line-height: 22px;
+        content: "the complete css framework";
     }
     `
 )
