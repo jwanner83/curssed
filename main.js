47bdea71d102edafd2b58b2bb58bbd5a0179b45b
Jonas Wanner <jonas@wanner.com>
2022-06-02 20:55:16 +0200

    feat: adding basic example usage

---
+++ main.js
@@ -0,0 +1,28 @@
+import { render } from './cssed.js'
+
+render(
+    document.querySelector('#app'),
+    `
+    .wrapper {
+        height: 400px;
+        width: 600px;
+        background: black;
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        padding: 40px;
+    } 
+    
+    .wrapper .title[h1] {
+        color: white;
+        content: "Hoihoi";
+        font-size: 100px;
+    }
+    
+    .wrapper .content[p] {
+        content: "Da isch so sau unnötig aber unglaublich lustig";
+        color: white;
+    }
+    `
+)
