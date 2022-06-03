ae737f9fa73a0ffc31d60d735bfa22347b656f1f
Jonas Wanner <jonas.wanner@clavisit.com>
2022-06-03 21:00:49 +0200

    feat: implement the error handler

---
+++ package/src/handlers/ErrorHandler.ts
@@ -0,0 +1,60 @@
+import { render } from '../mod'
+
+export default class ErrorHandler {
+
+  /**
+   * The default error message title
+   * @private
+   */
+  private static DEFAULT_TITLE = 'failed to render'
+
+  /**
+   * Displays an error message.
+   * @param title
+   * @param message
+   */
+  public displayError(message, title = ErrorHandler.DEFAULT_TITLE) {
+    render(
+      document.body,
+      {
+        markup: {
+          content:
+            `
+            .wrapper[div] {
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100vw;
+                height: 100vh;
+                display: flex;
+                justify-content: center;
+                align-items: center;   
+            }
+            
+            .wrapper .content {
+                width: 600px;
+                padding: 30px 30px 26px;
+                border: 3px solid red;
+                border-radius: 5px;
+                background: #fff5f5;
+                font-family: monospace;
+            }
+            
+            .wrapper .content .title[h1] {
+                color: red;
+                margin-top: 8px;
+                content: "${title}";
+            }
+            
+            .wrapper .content .message[p] {
+                color: #ff6565;
+                margin-bottom: 0;
+                line-height: 24px;
+                content: "${message}";
+            }
+            `
+        }
+      }
+    ).then()
+  }
+}
