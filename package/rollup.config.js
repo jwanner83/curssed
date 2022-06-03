9c6f00f7febf625e9b5b6e795b4181b437996ed1
Jonas Wanner <jonas.wanner@clavisit.com>
2022-06-03 21:25:29 +0200

    feat: fix one build behind

--- package/rollup.config.js
+++ package/rollup.config.js
@@ -4,24 +4,33 @@ import del from 'rollup-plugin-delete'
 
 const production = !process.env.ROLLUP_WATCH
 
-export default [{
-  input: 'src/mod.ts',
-  plugins: [
-    typescript({ tsconfig: './tsconfig.json', outputToFilesystem: false })
-  ],
-  output: {
-    file: 'dist/mod.js'
-  }
-}, {
-  input: 'dist/types/mod.d.ts',
-  plugins: [
-    dts(),
-    production && del({ targets: ['dist/types'], hook: 'buildEnd' })
-  ],
-  output: [
-    {
-      file: 'dist/mod.d.ts',
+const config = [
+  {
+    input: 'src/mod.ts',
+    plugins: [
+      typescript({ tsconfig: './tsconfig.json', outputToFilesystem: false })
+    ],
+    output: {
+      file: 'dist/mod.js',
       format: 'esm'
     }
-  ]
-}]
+  }
+]
+
+if (production) {
+  config.push({
+    input: 'dist/types/mod.d.ts',
+    plugins: [
+      dts(),
+      del({ targets: ['dist/types'], hook: 'buildEnd' })
+    ],
+    output: [
+      {
+        file: 'dist/mod.d.ts',
+        format: 'esm'
+      }
+    ]
+  })
+}
+
+export default config
