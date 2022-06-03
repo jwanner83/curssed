1e3a71846ea60a5058a5808ce0a30c08f5775102
Jonas Wanner <jonas.wanner@clavisit.com>
2022-06-03 21:01:49 +0200

    feat: add all handlers and implement process

--- package/src/mod.ts
+++ package/src/mod.ts
@@ -1,12 +1,29 @@
 import { CurssedRenderOptions } from './types/Curssed.types'
 import InputHandler from './handlers/InputHandler'
+import StyleHandler from './handlers/StyleHandler'
+import ASTHandler from './handlers/ASTHandler'
+import ErrorHandler from './handlers/ErrorHandler'
 
 export async function render (element: HTMLElement, options: CurssedRenderOptions) {
+  const errorHandler = new ErrorHandler()
   const inputHandler = new InputHandler()
+  const styleHandler = new StyleHandler()
+  const astHandler = new ASTHandler()
 
   try {
     const markup = await inputHandler.resolveContent(options.markup)
+    const curssed = styleHandler.getRules(markup)
+    const ast = astHandler.resolveAST(curssed)
+    const node = astHandler.convertASTToNode(ast)
+
+    document.head.appendChild(styleHandler.renderCSS(markup))
+    if (options.css) {
+      const css = await inputHandler.resolveContent(options.css)
+      document.head.appendChild(styleHandler.renderCSS(css))
+    }
+
+    element.appendChild(node)
   } catch (error) {
-    console.error(error)
+    errorHandler.displayError(error.message)
   }
 }
