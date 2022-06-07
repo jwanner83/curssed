6d462468c45c18d29ca0aaf3847073e6352ba3d8
Jonas Wanner <jonas.wanner@clavisit.com>
2022-06-07 22:40:11 +0200

    feat: adding runtime sample

---
+++ samples/runtime/main.js
@@ -0,0 +1,10 @@
+import { render } from './node_modules/curssed/dist/curssed.runtime.js'
+
+render(document.body, {
+  markup: {
+    file: 'markup.css'
+  },
+  css: {
+    file: 'style.css'
+  }
+}).then()
